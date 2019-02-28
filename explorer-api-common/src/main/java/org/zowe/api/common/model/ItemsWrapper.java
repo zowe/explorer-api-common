02f63b650539497bf13d530b895dd3b28acfce4b
stevenhorsman <steven@uk.ibm.com>
2019-02-28 11:00:16 +0000

    Remove un-uneeded lombok parameters
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- explorer-api-common/src/main/java/org/zowe/api/common/model/ItemsWrapper.java
+++ explorer-api-common/src/main/java/org/zowe/api/common/model/ItemsWrapper.java
@@ -10,15 +10,11 @@
 package org.zowe.api.common.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-@Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class ItemsWrapper<T> {
 
