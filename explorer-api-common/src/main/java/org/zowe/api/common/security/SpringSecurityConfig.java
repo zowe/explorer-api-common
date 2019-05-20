62884af988603ed2604cea4ed5a46e239a74c17f
Craig-Forrest <Craig.Forrest@ibm.com>
2019-05-20 09:29:14 +0100

    add logout endpoint with matcher
    Signed-off-by: Craig-Forrest <Craig.Forrest@ibm.com>

--- explorer-api-common/src/main/java/org/zowe/api/common/security/SpringSecurityConfig.java
+++ explorer-api-common/src/main/java/org/zowe/api/common/security/SpringSecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -35,7 +36,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // TODO - re-enable csrf?
         http.csrf().disable().authorizeRequests().antMatchers(AUTH_WHITELIST).permitAll().antMatchers("/api/**/*")
-                .authenticated().and().httpBasic().authenticationEntryPoint(authEntryPoint);
+                .authenticated().and().httpBasic().authenticationEntryPoint(authEntryPoint).and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/api/v1/**/logout"));
     }
 
     @Autowired
