67e5d8e555a86410163f14c1c2331053808e3e69
Jordan Cain <jordan.cain1@uk.ibm.com>
2020-03-27 14:30:16 -0400

    add back username endpoint
    Signed-off-by: Jordan Cain <jordan.cain1@uk.ibm.com>

---
+++ explorer-api-common/src/main/java/org/zowe/api/common/utils/ZosUtils.java
@@ -0,0 +1,23 @@
+/*  
+ * This program and the accompanying materials are made available under the terms of the    
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at 
+ * https://www.eclipse.org/legal/epl-v20.html   
+ *  
+ * SPDX-License-Identifier: EPL-2.0 
+ *  
+ * Copyright IBM Corporation 2018, 2020
+ */ 
+package org.zowe.api.common.utils;  
+
+import org.springframework.security.core.Authentication;    
+import org.springframework.security.core.context.SecurityContextHolder; 
+import org.zowe.api.common.security.CustomUser; 
+
+public class ZosUtils { 
+
+    public static String getUsername() {    
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication(); 
+        CustomUser customUser = (CustomUser) authentication.getPrincipal(); 
+        return customUser.getUsername().toUpperCase();  
+    }   
+}
