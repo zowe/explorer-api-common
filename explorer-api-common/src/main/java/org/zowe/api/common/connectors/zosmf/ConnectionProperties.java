3a6078bd55d3e4684440acebd1021adf9b21a459
Nakul Manchanda <nakul.manchanda@ibm.com>
2020-04-08 14:52:40 -0400

    add separate gateway port, and some style fixes
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- explorer-api-common/src/main/java/org/zowe/api/common/connectors/zosmf/ConnectionProperties.java
+++ explorer-api-common/src/main/java/org/zowe/api/common/connectors/zosmf/ConnectionProperties.java
@@ -9,11 +9,11 @@
  */
 package org.zowe.api.common.connectors.zosmf;
 
+import lombok.Data;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-
-import lombok.Data;
 
 @Data
 @Configuration
